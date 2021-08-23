06d0d4eaa972407df2687635f74a4b38edcdbeed
Daniel <dfirestone21@gmail.com>
2021-08-23 11:04:38 -0700

    Created Adapters for FlashcardsSetup recyclerview and Flashcards RecyclerView
    they're still empty

---
+++ app/src/main/java/com/example/mycheesecakes/ui/flashcards/FlashcardsAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.mycheesecakes.ui.flashcards
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mycheesecakes.R
+
+class FlashcardsAdapter() : RecyclerView.Adapter<FlashcardsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/mycheesecakes/ui/flashcards/FlashcardsSetupAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.mycheesecakes.ui.flashcards
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mycheesecakes.model.Dessert
+
+class FlashcardsSetupAdapter(private val dessertsList: List<Dessert>) : RecyclerView.Adapter<FlashcardsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlashcardsAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FlashcardsAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
