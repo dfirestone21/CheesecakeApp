4062a320fad5026ad3100d2ee4801e705012b797
Daniel <dfirestone21@gmail.com>
2021-11-28 12:36:55 -0800

    Created QuizRepository
    1. changed annotationProccessor to kapt in app\build.gradle for Room to compile
    
    2. Referenced the App class in Android Manifest
    
    3. Created the App class which holds references to QuizDatabase, MenuItemRepository, and QuizRepository.

--- app/src/main/java/com/example/mycheesecakes/app/App.kt
+++ app/src/main/java/com/example/mycheesecakes/app/App.kt
@@ -1,8 +1,48 @@
 package com.example.mycheesecakes.app
 
 import android.app.Application
+import com.example.mycheesecakes.data.cache.QuizDatabase
+import com.example.mycheesecakes.data.cache.RoomCache
+import com.example.mycheesecakes.data.network.RetrofitInstance
+import com.example.mycheesecakes.data.network.api.model.mappers.*
+import com.example.mycheesecakes.data.repositories.MenuItemRepository
+import com.example.mycheesecakes.data.repositories.QuizRepository
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+//@HiltAndroidApp
 class App: Application() {
+
+    companion object {
+        private lateinit var instance: App
+
+        private val database: QuizDatabase by lazy {
+            QuizDatabase.buildDatabase(instance)
+        }
+
+        val menuItemRepository: MenuItemRepository by lazy {
+            MenuItemRepository(
+                RetrofitInstance.airtableApi,
+                ApiCheesecakeMapper(),
+                ApiDessertMapper(),
+                ApiDrinkMapper()
+            )
+        }
+
+        val quizRepository: QuizRepository by lazy {
+            QuizRepository(
+                RetrofitInstance.airtableApi,
+                RoomCache(
+                    database.QuestionAnswerDao(),
+                    database.QuestionDao(),
+                    database.QuizDao(),
+                    database.QuizResultDao()),
+                QuizResultToApiQuizResultMapper()
+            )
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 }
